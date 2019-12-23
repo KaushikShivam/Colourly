71b9fd91ca84e172fae017b54c23de36c5c06993
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-24 03:13:26 +0530

    Configure mongodb database with express

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const dotenv = require('dotenv');
-
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Database Connected'));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`App started on port: ${PORT}`));
