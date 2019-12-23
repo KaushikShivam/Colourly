e3e806c96c058f235fffa7d4df976a13b1ce4e5e
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-23 18:17:41 +0530

    Add base Palette

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Palette from './components/Palette';
+import seedColors from './seedColors';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Palette {...seedColors[4]} />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Palette.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Palette = () => {
+  return (
+    <div className="Palette">
+      {/* navbar */}
+      <div className="Palette-colors">{/* Colorboxes */}</div>
+      {/* footer */}
+    </div>
+  );
+};
+
+export default Palette;
