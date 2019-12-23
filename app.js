f0cd8931f10183cb5936e68bc8c7535942c02eda
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-24 02:49:33 +0530

    Configure express app for deployment

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
@@ -10,5 +11,17 @@ if (process.env.NODE_ENV === 'development') {
 
 // Middleware for body parser
 app.use(express.json({ limit: '10kb' }));
+
+// All the routes will go here
+
+// Serve static assets in production - React
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder for public folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
