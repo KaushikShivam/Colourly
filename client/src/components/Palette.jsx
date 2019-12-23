93aad5855244698bcb58b0dc2231ba60b2f96e8a
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-24 01:20:26 +0530

    Style navbar component

--- client/src/components/Palette.jsx
+++ client/src/components/Palette.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import ColorBox from './ColorBox';
-import Slider from 'rc-slider';
-
-import 'rc-slider/assets/index.css';
+import Navbar from './Navbar';
 import './Palette.css';
 
 const Palette = ({ palette: { colors } }) => {
@@ -12,18 +10,11 @@ const Palette = ({ palette: { colors } }) => {
     <ColorBox background={color.hex} name={color.name} />
   ));
 
+  const changeLevel = level => setLevel(level);
+
   return (
     <div className="Palette">
-      <div className="slider">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onChange={newLevel => setLevel(newLevel)}
-        />
-      </div>
-      {/* navbar */}
+      <Navbar level={level} changeLevel={changeLevel} />
       <div className="Palette-colors">{colorBoxes}</div>
       {/* footer */}
     </div>
