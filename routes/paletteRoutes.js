d30a674076af29c81b2d9c9a36be5a203440dedd
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-24 04:17:42 +0530

    Mount palette controller functions on palette router

--- routes/paletteRoutes.js
+++ routes/paletteRoutes.js
@@ -1,5 +1,16 @@
 const express = require('express');
-
+const paletteController = require('./../controllers/paletteController');
 const router = express.Router();
+
+router
+  .route('/')
+  .get(paletteController.getAllPalettes)
+  .post(paletteController.createPalette);
+
+router
+  .route('/:id')
+  .get(paletteController.getPalette)
+  .patch(paletteController.updatePalette)
+  .delete(paletteController.deletePalette);
 
 module.exports = router;
