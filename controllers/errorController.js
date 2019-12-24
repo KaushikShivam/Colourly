b1ddc89ec5ed12b22dd8847c2e0873e078734ed2
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-24 16:48:50 +0530

    Configure prod and dev error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,39 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    // Operational error that we trust
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    // Programming error or some unknown error. So we send a generic message instead of leaking anything to the client
+    // 1. Log the error that will show up in the heroku logs for dev
+    console.error('ERROR', err);
+
+    // 2. Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Oops! Something went wrong. Please try again later'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
