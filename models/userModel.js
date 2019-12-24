420f4ed8b57ac7cb800549a6234b3b26bbc98a16
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-24 21:30:09 +0530

    Hash the password with bcrypt, delete passwordConfirm

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -22,8 +23,26 @@ const userSchema = mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm the password']
+    required: [true, 'Please confirm the password'],
+    validate: {
+      // This only works on CREATE and SAVE
+      validator: function(el) {
+        return this.password === el;
+      },
+      message: 'The passwords do not match'
+    }
   }
+});
+
+userSchema.pre('save', async function(next) {
+  // Only run this function if the password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete the password cinfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
