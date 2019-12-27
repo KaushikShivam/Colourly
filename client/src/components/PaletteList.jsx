a1bec28bf4ac645c0419a85a7b07ce27ff55d5f4
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-27 17:54:30 +0530

    Add base markup to the PaletteList

--- client/src/components/PaletteList.jsx
+++ client/src/components/PaletteList.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
 
 import MiniPalette from './MiniPalette';
 
-const PaletteList = ({ palettes }) => {
+const styles = {
+  root: {
+    backgroundColor: 'blue'
+  }
+};
+
+const PaletteList = ({ palettes, classes }) => {
   return (
-    <div>
-      <MiniPalette />
-      {palettes.map(palette => (
-        <MiniPalette {...palette} />
-      ))}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        {/* TODO: Refactor and make this a global NAV */}
+        <nav className={classes.nav}>
+          <h1>Colourly</h1>
+        </nav>
+        <div className={classes.palettes}>
+          {palettes.map(palette => (
+            <MiniPalette {...palette} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
