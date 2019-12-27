0addc48e34daddafeaddb7d2aeb6a88caceb3887
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-27 14:48:21 +0530

    Integrate react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Palette from './components/Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './helpers/colorHelpers';
 
 function App() {
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+      <Route
+        exact
+        path="palettes/:id"
+        render={() => <h1>Individual palette goes here</h1>}
+      />
+    </Switch>
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
