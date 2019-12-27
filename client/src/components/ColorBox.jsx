65a587e48e5b999027b2009b931b145ea9bf9218
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-27 23:44:24 +0530

    Refactor colorbox styles

--- client/src/components/ColorBox.jsx
+++ client/src/components/ColorBox.jsx
@@ -2,10 +2,65 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import chroma from 'chroma-js';
+import { withStyles } from '@material-ui/styles';
 import './ColorBox.css';
 
-const ColorBox = ({ background, name, moreUrl, showLink }) => {
+const styles = {
+  ColorBox: {
+    width: '20%',
+    height: props => (!props.showLink ? '50%' : '25%'),
+    margin: '0 auto',
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+    marginBottom: '-3.5px',
+    '&:hover button': {
+      opacity: '1'
+    }
+  },
+  seeMore: {
+    background: 'rgba(255, 255, 255, 0.3)',
+    position: 'absolute',
+    border: 'none',
+    right: '0',
+    bottom: '0',
+    color: 'rgba(0, 0, 0, 0.726)',
+    width: '60px',
+    height: '30px',
+    textAlign: 'center',
+    lineHeight: '30px',
+    textTransform: 'uppercase'
+  },
+  copyButton: {
+    width: '100px',
+    height: '30px',
+    position: 'absolute',
+    display: 'inline-block',
+    top: '50%',
+    left: '50%',
+    marginLeft: '-50px',
+    marginTop: '-15px',
+    textAlign: 'center',
+    outline: 'none',
+    background: 'rgba(255, 255, 255, 0.3)',
+    fontSize: '1rem',
+    lineHeight: '30px',
+    color: 'black',
+    textTransform: 'uppercase',
+    border: 'none',
+    textDecoration: 'none',
+    opacity: '0'
+  },
+  whiteColor: {
+    color: 'white'
+  }
+};
+
+const ColorBox = ({ background, name, moreUrl, showLink, classes }) => {
   const [copied, setCopied] = useState(false);
+
+  const contrast =
+    chroma.contrast(background, 'black') < 6 && classes.whiteColor;
 
   const changeCopyState = () => {
     setCopied(true);
@@ -14,32 +69,29 @@ const ColorBox = ({ background, name, moreUrl, showLink }) => {
     }, 1500);
   };
 
-  const contrast = chroma.contrast(background, 'black') < 6;
-
   return (
     <CopyToClipboard text={background} onCopy={changeCopyState}>
-      <div style={{ background }} className="ColorBox">
+      <div style={{ background }} className={classes.ColorBox}>
         <div
           style={{ background }}
           className={`copy-overlay ${copied && 'show'}`}
         ></div>
-        <div className={`copy-msg ${copied && 'show'}`}>
+        <div
+          className={`copy-msg ${copied && 'show'} ${contrast &&
+            'white-color'}`}
+        >
           <h1>copied!</h1>
           <p>{background}</p>
         </div>
         <div className="copy-container">
           <div className="box-content">
-            <span className={contrast && 'white-color'}>{name}</span>
+            <span className={contrast}>{name}</span>
           </div>
-          <button className={`copy-button ${contrast && 'white-color'}`}>
-            Copy
-          </button>
+          <button className={`${classes.copyButton} ${contrast}`}>Copy</button>
         </div>
         {showLink && (
           <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-            <span className={`see-more ${contrast && 'white-color'}`}>
-              MORE
-            </span>
+            <span className={`${classes.seeMore} ${contrast}`}>MORE</span>
           </Link>
         )}
       </div>
@@ -47,4 +99,4 @@ const ColorBox = ({ background, name, moreUrl, showLink }) => {
   );
 };
 
-export default ColorBox;
+export default withStyles(styles)(ColorBox);
