ba7bf1ab5bbc3d7c2c297c4af21282341fad8431
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-12-28 22:33:42 +0530

    Style draggable color box

--- client/src/styles/ColorBox.styles.js
+++ client/src/styles/ColorBox.styles.js
@@ -49,14 +49,14 @@ export default {
   },
   boxContent: {
     position: 'absolute',
-    padding: 10,
+    padding: '10px',
     width: '100%',
-    left: 0,
-    bottom: 0,
-    color: 'black',
-    letterSpacing: 1,
+    left: '0',
+    bottom: '0',
+    color: 'white',
+    letterSpacing: '1px',
     textTransform: 'uppercase',
-    fontSize: 12
+    fontSize: '12px'
   },
   copyOverlay: {
     opacity: 0,
