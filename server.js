c195565147e7db1766692f8e4db04791aeb81e2c
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-06 17:30:19 +0530

    Configure mongoose

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: 'config.env' });
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`App started on PORT: ${PORT}`));
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => 'DB Connected');
