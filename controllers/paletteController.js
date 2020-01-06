2f462d44357ad9de50359624d090a43cd4e6e5f6
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-06 17:08:39 +0530

    Create palette create handler

--- controllers/paletteController.js
+++ controllers/paletteController.js
@@ -1,3 +1,19 @@
 const Palette = require('./../models/paletteModel');
 
-const createPalette = async (req, res, next) => {};
+const createPalette = async (req, res, next) => {
+  try {
+    const palette = await Palette.create(req.body);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        palette
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
