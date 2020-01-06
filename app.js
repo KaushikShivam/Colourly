700390179510faa80f2a27397c357f6bf17f58c0
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-06 17:17:19 +0530

    Add morgan to req logging

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // Get all routes
 const paletteRouter = require('./routes/paletteRoute');
 
 const app = express();
+
+// Morgan for logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // body parser
 app.use(express.json());
