8fe79dd0d7023b52633992ecca5ad965a7f12543
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-06 18:55:09 +0530

    Add deletePalette handler

--- controllers/paletteController.js
+++ controllers/paletteController.js
@@ -61,6 +61,52 @@ exports.getPalette = async (req, res, next) => {
   }
 };
 
-exports.updatePalette = async (req, res, next) => {};
+exports.updatePalette = async (req, res, next) => {
+  try {
+    const palette = await Palette.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
 
-exports.deletePalette = async (req, res, next) => {};
+    if (!palette) {
+      return res.status(400).json({
+        status: 'fail',
+        message: err
+      });
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        palette
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
+exports.deletePalette = async (req, res, next) => {
+  try {
+    const palette = await Palette.findByIdAndDelete(req.params.id);
+    if (!palette) {
+      return res.status(400).json({
+        status: 'fail',
+        message: err
+      });
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    return res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
