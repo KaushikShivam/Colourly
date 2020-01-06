01f5a70d68ab47d93b101b7e839cb2007feb9994
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-06 17:50:18 +0530

    Add getPalette handler function

--- controllers/paletteController.js
+++ controllers/paletteController.js
@@ -4,7 +4,7 @@ exports.createPalette = async (req, res, next) => {
   try {
     const palette = await Palette.create(req.body);
 
-    res.status(200).json({
+    res.status(201).json({
       status: 'success',
       data: {
         palette
@@ -35,3 +35,32 @@ exports.getAllPalettes = async (req, res, next) => {
     });
   }
 };
+
+exports.getPalette = async (req, res, next) => {
+  try {
+    const palette = await Palette.findById(req.params.id);
+
+    if (!palette) {
+      return res.status(400).json({
+        status: 'fail',
+        message: err
+      });
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        palette
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
+exports.updatePalette = async (req, res, next) => {};
+
+exports.deletePalette = async (req, res, next) => {};
