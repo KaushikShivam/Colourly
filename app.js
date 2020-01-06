02a566e2c396dfb26d988d212fe59f67b2e09fb9
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-06 17:13:47 +0530

    Add palette routes to appjs, mount them to express app

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
+// Get all routes
+const paletteRouter = require('./routes/paletteRoute');
+
 const app = express();
 
 // body parser
 app.use(express.json());
+
+// Mount all routers
+app.use('/api/v1/palettes', paletteRouter);
 
 module.exports = app;
