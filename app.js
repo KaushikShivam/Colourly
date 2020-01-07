c6e646ba1505a4c17d7227e5199132a3651659e1
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-01-07 19:46:35 +0530

    Implement error handling middleware, Sent error from catch all route

--- app.js
+++ app.js
@@ -17,4 +17,22 @@ app.use(express.json());
 // Mount all routers
 app.use('/api/v1/palettes', paletteRouter);
 
+// Add handler for uncaught routes
+app.all('*', (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
+
 module.exports = app;
