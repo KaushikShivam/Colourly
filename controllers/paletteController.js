e7fcdbfe37b06a7b12501a71d4a36eb2ac244e08
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-26 21:13:02 +0530

    Create palette controller

--- controllers/paletteController.js
+++ controllers/paletteController.js
@@ -1,26 +1,8 @@
 const Palette = require('./../models/paletteModel');
 
-exports.createPalette = async (req, res, next) => {
-  try {
-    const palette = await Palette.create(req.body);
-
-    res.status(201).json({
-      status: 'success',
-      data: {
-        palette
-      }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err
-    });
-  }
-};
-
 exports.getAllPalettes = async (req, res, next) => {
   try {
-    const palettes = await Palette.find().sort('-createdAt');
+    const palettes = await Palette.find();
 
     res.status(200).json({
       status: 'success',
@@ -55,6 +37,24 @@ exports.getPalette = async (req, res, next) => {
     });
   } catch (err) {
     res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
+exports.createPalette = async (req, res, next) => {
+  try {
+    const palette = await Palette.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        palette
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
       status: 'fail',
       message: err
     });
