509a89fb60e656485dbeb56e56be828e39b747dc
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-26 21:37:04 +0530

    Add route for unhandled requests

--- app.js
+++ app.js
@@ -13,4 +13,11 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/palettes', paletteRouter);
 
+app.use('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
+
 module.exports = app;
