6fdeb0268907aa16a173f21a2b24aed694a8cb93
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-03-28 00:00:54 +0530

    Add error handler

--- app.js
+++ app.js
@@ -13,10 +13,20 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/palettes', paletteRouter);
 
+// Handle unhandled routes
 app.use('*', (req, res) => {
   res.status(404).json({
     status: 'fail',
     message: `Can't find ${req.originalUrl} on this server`
+  });
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
