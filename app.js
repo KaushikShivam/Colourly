424f2c15e6e06202009f8468085fd3ba8e56c85e
shivam <shivam.kaushik@outlook.com>
2020-03-28 17:11:38 +0530

    Add security to app

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/AppError');
@@ -13,6 +18,35 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Rate limiting
+const limiter = rateLimit({
+  max: 100, // maximum requests
+  windowMs: 60 * 60 * 1000, // in how many hours?
+  message: 'Too many requests from this IP. Please try again in an hour'
+});
+
+// Only limiting our api
+app.use('/api', limiter);
+
+// Set secure http headers on api
+app.use(helmet());
+
+// body parser with data limit
+app.use(
+  express.json({
+    limit: '10kb'
+  })
+);
+
+// Data sanitization against NoSql query injection
+// will look at request body, params and query string and filter out all of the dollar signs and dots
+app.use(mongoSanitize());
+// Data sanitization against cross site scripting attacks
+app.use(xss());
+
+// parameter pollution prevent
+app.use(hpp());
 
 app.use('/api/v1/palettes', paletteRouter);
 app.use('/api/v1/users', userRouter);
