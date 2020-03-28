cdee177b79f02aa6cf5c8a9ebce182b15b8ee147
shivam <shivam.kaushik@outlook.com>
2020-03-28 13:00:34 +0530

    Add catchAsync handler to catch errors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/AppError');
 // Get all routes
 const paletteRouter = require('./routes/paletteRoute');
 
@@ -15,19 +17,9 @@ app.use('/api/v1/palettes', paletteRouter);
 
 // Handle unhandled routes
 app.use('*', (req, res) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
