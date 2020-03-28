71ce002685b2a8c12994fcaadf3557f60483ee85
shivam <shivam.kaushik@outlook.com>
2020-03-28 12:49:27 +0530

    Add ErrorController to export global error handler

--- utils/AppError.js
+++ utils/AppError.js
@@ -2,7 +2,7 @@ class AppError extends error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error’;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
