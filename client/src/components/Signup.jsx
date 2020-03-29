abdebf7f6d883a9c52c6ab167d10dc5d059b6027
shivam <shivam.kaushik@outlook.com>
2020-03-29 12:47:26 +0530

    Add Header to the register page

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,66 +1,71 @@
 import React from 'react';
 
 import './../styles/Signup.css';
+import Header from './Header';
 
 const Signup = () => {
   return (
-    <main className="main">
-      <div className="login-form">
-        <h2 className="heading-secondary ma-bt-lg">Create your account</h2>
-        <form className="form form--signup">
-          <div className="form__group">
-            <label htmlFor="name" className="form__label">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              className="form__input"
-              placeholder="John doe"
-              required
-            />
-          </div>
-          <div className="form__group">
-            <label htmlFor="email" className="form__label">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="form__input"
-              placeholder="you@example.com"
-              required
-            />
-          </div>
-          <div className="form__group ma-bt-md">
-            <label htmlFor="password" className="form__label">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="form__input"
-              placeholder="••••••••"
-              minLength="8"
-              required
-            />
-          </div>
-          <div className="form__group ma-bt-md">
-            <label htmlFor="passwordConfirm" className="form__label">
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="passwordConfirm"
-              className="form__input"
-              placeholder="••••••••"
-              minLength="8"
-              required
-            />
-          </div>
-        </form>
-      </div>
-    </main>
+    <>
+      <Header />
+
+      <main className="main">
+        <div className="login-form">
+          <h2 className="heading-secondary ma-bt-lg">Create your account</h2>
+          <form className="form form--signup">
+            <div className="form__group">
+              <label htmlFor="name" className="form__label">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="form__input"
+                placeholder="John doe"
+                required
+              />
+            </div>
+            <div className="form__group">
+              <label htmlFor="email" className="form__label">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="form__input"
+                placeholder="you@example.com"
+                required
+              />
+            </div>
+            <div className="form__group ma-bt-md">
+              <label htmlFor="password" className="form__label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form__input"
+                placeholder="••••••••"
+                minLength="8"
+                required
+              />
+            </div>
+            <div className="form__group ma-bt-md">
+              <label htmlFor="passwordConfirm" className="form__label">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="passwordConfirm"
+                className="form__input"
+                placeholder="••••••••"
+                minLength="8"
+                required
+              />
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
   );
 };
 
