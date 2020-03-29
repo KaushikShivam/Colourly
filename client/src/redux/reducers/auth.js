83bc30f323285d8db102a0f7041482610866b171
shivam <shivam.kaushik@outlook.com>
2020-03-29 16:39:49 +0530

    Add auth reducer

---
+++ client/src/redux/reducers/auth.js
@@ -0,0 +1,43 @@
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT
+} from './../actions/types';
+
+const INITIAL_STATE = {
+  user: null,
+  isAuthenticated: null,
+  loading: true,
+  user: null
+};
+
+const authReducer = (state = INITIAL_STATE, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+      localStorage.setItem('jwt', payload);
+      return {
+        ...state,
+        token: payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case AUTH_ERROR:
+    case LOGOUT:
+      localStorage.removeItem('jwt');
+      return { ...state, token: null, isAuthenticated: false, loading: false };
+    case USER_LOADED:
+      return { ...state, isAuthenticated: true, loading: false, user: payload };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
