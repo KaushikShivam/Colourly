49e61e871879cc7a8988e7c80acaec9daa8e9b94
shivam <shivam.kaushik@outlook.com>
2020-03-30 14:14:54 +0530

    Add user ref on palettemodel

--- models/paletteModel.js
+++ models/paletteModel.js
@@ -11,6 +11,11 @@ const paletteSchema = new mongoose.Schema(
       minlength: 10
     },
     slug: String,
+    user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User',
+      required: [true, ' A palette should always have a user']
+    },
     colors: {
       type: [
         {
@@ -28,7 +33,9 @@ const paletteSchema = new mongoose.Schema(
     }
   },
   {
-    timestamps: true
+    timestamps: true,
+    toJSON: { virtuals: true }, // Needed for virtual populate
+    toObject: { virtuals: true }
   }
 );
 
